fd261dedadab484e3e3f3ee3d664192c4338407f
Elio E. Duarte <elio.esteves.duarte@gmail.com>
2016-01-24 14:21:10 -0200

    update version

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     name='tomate-launcher-plugin',
     data_files=find_data_files(DATA_FILES, 'tomate'),
     url='https://github.com/eliostvs/tomate-launcher-plugin',
-    version='0.4.1',
+    version='0.5.0',
     zip_safe=False,
 )
