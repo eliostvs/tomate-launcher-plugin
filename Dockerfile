9544c60b7615d5191cc18954733dbd96fc1dcacf
Elio E. Duarte <eduarte@thoughtworks.com>
2016-04-17 20:22:40 -0300

    using pytest

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,8 @@
 FROM eliostvs/tomate
 
-ENV PROJECT /code/
-
-COPY ./ $PROJECT
-
 RUN apt-get update -qq && apt-get -yqq install dbus-x11 gir1.2-unity-5.0
 
-WORKDIR $PROJECT
+WORKDIR /code/
 
 ENTRYPOINT ["make"]
 
--- test.py
+++ test.py
@@ -1,62 +1,74 @@
 from __future__ import unicode_literals
 
-import unittest
-
+import pytest
 from mock import Mock, patch
 from tomate.constant import State
 from tomate.graph import graph
 
 
-class TestLauncherPlugin(unittest.TestCase):
+@pytest.fixture()
+@patch('gi.repository.Unity.LauncherEntry')
+def plugin(launcher_entry):
+    graph.register_factory('tomate.session', Mock)
 
-    @patch('gi.repository.Unity.LauncherEntry')
-    def setUp(self, launcher_entry):
-        graph.register_factory('tomate.session', Mock)
+    from launcher_plugin import LauncherPlugin
 
-        from launcher_plugin import LauncherPlugin
+    return LauncherPlugin()
 
-        self.plugin = LauncherPlugin()
-        self.launcher_entry = launcher_entry
 
-    def test_get_tomate_desktop_id(self):
-        self.launcher_entry.get_for_desktop_id.assert_called_with('tomate-gtk.desktop')
+@patch('gi.repository.Unity.LauncherEntry')
+def test_get_tomate_desktop_id(launcher_entry):
+    graph.register_factory('tomate.session', Mock)
 
-    def test_should_show_count_when_activate_and_pomodoro_is_stopped(self):
-        self.plugin.session.status.return_value = {'state': State.stopped, 'sessions': 2}
-        self.plugin.activate()
+    from launcher_plugin import LauncherPlugin
 
-        self.plugin.launcher.set_property.assert_any_call('count', 2)
-        self.plugin.launcher.set_property.assert_any_call('count_visible', True)
+    LauncherPlugin()
 
-    def test_should_show_progress_whem_activate_and_pomodoro_is_running(self):
-        self.plugin.session.status.return_value = {'state': State.started, 'sessions': 2}
-        self.plugin.activate()
+    launcher_entry.get_for_desktop_id.assert_called_with('tomate-gtk.desktop')
 
-        self.plugin.launcher.set_property.assert_any_call('progress_visible', True)
 
-    def test_should_hide_launcher_and_progress_when_deactivate(self):
-        self.plugin.deactivate()
+def test_should_show_count_when_activate_and_pomodoro_is_stopped(plugin):
+    plugin.session.status.return_value = {'state': State.stopped, 'sessions': 2}
 
-        self.plugin.launcher.set_property.assert_any_call('progress_visible', False)
-        self.plugin.launcher.set_property.assert_any_call('count_visible', False)
+    plugin.activate()
 
-    def test_should_hide_count_and_show_progress_when_session_started(self):
-        self.plugin.on_session_started()
+    plugin.launcher.set_property.assert_any_call('count', 2)
+    plugin.launcher.set_property.assert_any_call('count_visible', True)
 
-        self.plugin.launcher.set_property.assert_any_call('progress_visible', True)
-        self.plugin.launcher.set_property.assert_any_call('progress', 0)
-        self.plugin.launcher.set_property.assert_any_call('count_visible', False)
 
-    def test_should_show_count_and_hide_progress_when_session_ended(self):
-        self.plugin.on_session_ended(sessions=4)
+def test_should_show_progress_whem_activate_and_pomodoro_is_running(plugin):
+    plugin.session.status.return_value = {'state': State.started, 'sessions': 2}
+    plugin.activate()
 
-        self.plugin.launcher.set_property.assert_any_call('progress_visible', False)
-        self.plugin.launcher.set_property.assert_any_call('count_visible', True)
-        self.plugin.launcher.set_property.assert_any_call('count', 4)
+    plugin.launcher.set_property.assert_any_call('progress_visible', True)
 
-    def test_should_update_progress_when_timer_updated(self):
-        self.plugin.update_progress()
-        self.plugin.launcher.set_property.assert_called_with('progress', 0)
 
-        self.plugin.update_progress(time_ratio=0.5)
-        self.plugin.launcher.set_property.assert_called_with('progress', 0.5)
+def test_should_hide_launcher_and_progress_when_deactivate(plugin):
+    plugin.deactivate()
+
+    plugin.launcher.set_property.assert_any_call('progress_visible', False)
+    plugin.launcher.set_property.assert_any_call('count_visible', False)
+
+
+def test_should_hide_count_and_show_progress_when_session_started(plugin):
+    plugin.on_session_started()
+
+    plugin.launcher.set_property.assert_any_call('progress_visible', True)
+    plugin.launcher.set_property.assert_any_call('progress', 0)
+    plugin.launcher.set_property.assert_any_call('count_visible', False)
+
+
+def test_should_show_count_and_hide_progress_when_session_ended(plugin):
+    plugin.on_session_ended(sessions=4)
+
+    plugin.launcher.set_property.assert_any_call('progress_visible', False)
+    plugin.launcher.set_property.assert_any_call('count_visible', True)
+    plugin.launcher.set_property.assert_any_call('count', 4)
+
+
+def test_should_update_progress_when_timer_updated(plugin):
+    plugin.update_progress()
+    plugin.launcher.set_property.assert_called_with('progress', 0)
+
+    plugin.update_progress(time_ratio=0.5)
+    plugin.launcher.set_property.assert_called_with('progress', 0.5)
