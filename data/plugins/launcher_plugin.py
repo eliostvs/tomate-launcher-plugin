270ab4d34241b3da190313b1f5e0fe94a3d86287
Elio Esteves Duarte <elio.esteves.duarte@gmail.com>
2021-04-07 12:29:18 -0300

    feat(plugin): new tomate API

--- data/plugins/launcher_plugin.py
+++ data/plugins/launcher_plugin.py
@@ -1,16 +1,16 @@
 import logging
 
 import gi
-from gi.repository import Unity
-
-from tomate.pomodoro import State
-from tomate.pomodoro.event import Events, on
-from tomate.pomodoro.graph import graph
-from tomate.pomodoro.plugin import suppress_errors, Plugin
-from tomate.pomodoro.session import Payload as SessionPayload
-from tomate.pomodoro.timer import Payload as TimerPayload
 
 gi.require_version("Unity", "7.0")
+
+from gi.repository import Unity
+
+from tomate.pomodoro.event import Events, on
+from tomate.pomodoro.graph import graph
+from tomate.pomodoro.plugin import Plugin, suppress_errors
+from tomate.pomodoro.session import Payload as SessionPayload
+from tomate.pomodoro.timer import Payload as TimerPayload
 
 logger = logging.getLogger(__name__)
 
@@ -19,14 +19,12 @@ class LauncherPlugin(Plugin):
     @suppress_errors
     def __init__(self):
         super(LauncherPlugin, self).__init__()
-
         self.widget = Unity.LauncherEntry.get_for_desktop_id("tomate-gtk.desktop")
         self.session = graph.get("tomate.session")
 
     @suppress_errors
     def activate(self):
         super(LauncherPlugin, self).activate()
-
         if self.session.is_running():
             self.enable_progress()
             self.disable_counter()
@@ -42,44 +40,46 @@ class LauncherPlugin(Plugin):
         self.disable_progress()
 
     def disable_counter(self):
+        logger.debug("action=disable_counter")
         self.widget.set_property("count_visible", False)
 
     @suppress_errors
-    @on(Events.Session, [State.started])
-    def on_session_started(self, *args, **kwargs):
+    @on(Events.SESSION_START)
+    def on_session_started(self, *_, **__):
         self.disable_counter()
         self.enable_progress()
 
     def enable_progress(self):
+        logger.debug("action=enable_progress")
         self.widget.set_property("progress", 0)
         self.widget.set_property("progress_visible", True)
 
     @suppress_errors
-    @on(Events.Session, [State.finished, State.stopped])
-    def on_session_ended(self, *args, payload: SessionPayload):
+    @on(Events.SESSION_END, Events.SESSION_INTERRUPT)
+    def on_session_ended(self, *_, payload: SessionPayload):
         self.disable_progress()
         self.enable_counter()
         self.update_counter(payload.pomodoros)
 
     def disable_progress(self):
+        logger.debug("action=disable_progress")
         self.widget.set_property("progress_visible", False)
 
     def enable_counter(self):
+        logger.debug("action=enable_counter")
         self.widget.set_property("count_visible", True)
 
     @suppress_errors
-    @on(Events.Timer, [State.changed])
+    @on(Events.TIMER_UPDATE)
     def update_progress(self, _, payload: TimerPayload):
+        logger.debug("action=update progress=%.1f", payload.elapsed_ratio)
         self.widget.set_property("progress", payload.elapsed_ratio)
 
-        logger.debug("launcher progress update to %.1f", payload.elapsed_ratio)
-
     @suppress_errors
-    @on(Events.Session, [State.reset])
-    def reset_counter(self, *args, **kwargs):
+    @on(Events.SESSION_RESET)
+    def reset_counter(self, *_, **__):
         self.update_counter(0)
 
     def update_counter(self, count: int):
+        logger.debug("action=update counter=%d", count)
         self.widget.set_property("count", count)
-
-        logger.debug("launcher count updated to %d", count)
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,28 +1,31 @@
 import pytest
+from blinker import NamedSignal
 
-from tomate.pomodoro import State, Sessions
 from tomate.pomodoro.event import Events
 from tomate.pomodoro.graph import graph
-from tomate.pomodoro.session import Session, Payload as SessionPayload
+from tomate.pomodoro.session import Payload as SessionPayload, Session, Type as SessionType
 from tomate.pomodoro.timer import Payload as TimerPayload
 
 
-@pytest.fixture()
+@pytest.fixture
+def bus():
+    return NamedSignal("Test")
+
+
+@pytest.fixture
 def session(mocker):
     return mocker.Mock(spec=Session)
 
 
-@pytest.fixture()
-def subject(session):
+@pytest.fixture
+def subject(bus, session):
     graph.providers.clear()
-
+    graph.register_instance("tomate.bus", bus)
     graph.register_instance("tomate.session", session)
 
-    Events.Session.receivers.clear()
-    Events.Timer.receivers.clear()
+    import launcher_plugin
 
-    from launcher_plugin import LauncherPlugin
-    return LauncherPlugin()
+    return launcher_plugin.LauncherPlugin()
 
 
 def test_active_plugin_when_session_is_not_running(session, subject):
@@ -58,48 +61,45 @@ def test_deactivate_plugin(subject):
     assert subject.widget.get_progress_visible() is False
 
 
-def test_enable_progress_when_session_start(session, subject):
+def test_enable_progress_when_session_start(bus, session, subject):
     subject.activate()
 
-    Events.Session.send(State.started)
+    bus.send(Events.SESSION_START)
 
     assert subject.widget.get_progress_visible() is True
     assert subject.widget.get_progress() == 0.0
     assert subject.widget.get_count_visible() is False
 
 
-@pytest.mark.parametrize("event", [State.finished, State.stopped])
-def test_show_counter_when_session_stop(event, subject):
+@pytest.mark.parametrize("event", [Events.SESSION_END, Events.SESSION_INTERRUPT])
+def test_show_counter_when_session_stop(event, bus, subject):
     subject.activate()
 
     payload = SessionPayload(
         id="1234",
-        state=event,
-        type=Sessions.pomodoro,
+        type=SessionType.POMODORO,
         duration=0,
         pomodoros=4,
     )
-
-    Events.Session.send(event, payload=payload)
+    bus.send(event, payload=payload)
 
     assert subject.widget.get_progress_visible() is False
     assert subject.widget.get_count_visible() is True
     assert subject.widget.get_count() == 4
 
 
-def test_update_progress_when_timer_change(session, subject):
+def test_updates_progress_when_timer_change(bus, session, subject):
     subject.activate()
 
-    payload = TimerPayload(time_left=5, duration=10)
-    Events.Timer.send(State.changed, payload=payload)
+    bus.send(Events.TIMER_UPDATE, payload=TimerPayload(time_left=5, duration=10))
 
     assert subject.widget.get_progress() == 0.5
 
 
-def test_reset_counter_when_session_reset(subject):
+def test_resets_counter_when_session_reset(bus, subject):
     subject.activate()
     subject.widget.set_count(4)
 
-    Events.Session.send(State.reset)
+    bus.send(Events.SESSION_RESET)
 
     assert subject.widget.get_count() == 0
